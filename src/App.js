733332edadce92b2640ca3285240fabeca893219
Cameron Fiske <cameronfiske@Camerons-MacBook-Pro.local>
2022-01-10 17:16:47 -0800

    clickable menu working and basic text working

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ function App() {
     }
   ])
 
+  const [display, setDisplay] = useState(1)
+
   function expandDesign (id){
     if (!designes.some(design => design.expanded === true))
     {
@@ -60,16 +62,28 @@ function App() {
 
   return (
     <div className='container'>
-      <div className='rBoarder'></div>
-      <div className='lBoarder'></div>
-      <div >
+      {/* <div className='rBoarder'></div>
+      <div className='lBoarder'></div> */}
+      {/* <div >
         <img src={svg} className='header'/>
         <p className='text'> la;ldskj fasdjflsdkfj ;laf ldhfkl jadsalkfj sdl;kfj a;lsdkfj ;aslkdfj ;ladskfj ;laksdf j;laksfj ;lakdsj f;laskdjf ;lkasdjf ;laksdjf ;laksdjf ;laksdjf ;lkadsjf ;laksdjf hflasdjhfkajsdh </p>
         <h2 className='title'>Flyer Gallary</h2>
-      </div>
-      
+      </div> */}
+      <div class='section_title' onClick={()=>(setDisplay(1))}><h1>FLIERCLUB</h1></div><br/>
+      {display === 1 ? <p>Hello. This is Flier Club. To submit a design of your own join our Discord. All official designs are published in Gallery. We spend a lot of time and effort making our fliers and wouldn’t want to see them go to waste. See Recycle to give your flier new life.</p> : ""}
+      <div class='section_title' onClick={()=>(setDisplay(2))}><h1>DISCORD</h1></div><br/>
+      {display === 2 ? <p>The Awesome</p> : ""}
+      <div class='section_title' onClick={()=>(setDisplay(3))}><h1>GALLERY</h1></div><br/>
+      {display === 3 ? <DesTable designes={designes} onClick={expandDesign}/> : ""}
+      <div class='section_title' onClick={()=>(setDisplay(4))}><h1>RECYCLE</h1></div><br/>
+      {display === 4 ? <ol>
+        <li>1. Regift/Display Flier</li>
+        <li>2. Fold worlds longest flying paper airplane</li>
+        <li>3. Create A Sitting Puppy</li>
+        <li>4. Make an envelope to hold a note to a friend.</li>
+        </ol>: ""}
   
-      <DesTable designes={designes} onClick={expandDesign}/>
+      
       {designes.map((design) => (
                 <Expanded trigger={design.expanded} design={design} onClose={closeDesign}/>
             ))}
