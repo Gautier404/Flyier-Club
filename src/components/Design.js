165690b0aa8998724cb5b669ab79fb4b3c203e71
Cameron Fiske <cameronfiske@Camerons-MacBook-Pro.local>
2022-01-21 23:00:52 -0800

    Media queries for touch
    This may not be the final itteration
    rn it just shows the overlay if you press it and
    it disappears when you press it again

--- src/components/Design.js
+++ src/components/Design.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import {useState} from 'react'
 import expandIcon from '../designes/expand.png'
 import '../Styles/Design.css'
 import download_icon from '../designes/download_icon.png'
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 
 const Design = ({design, onClick}) => {
@@ -9,25 +11,45 @@ const Design = ({design, onClick}) => {
     //     img.style.transform = "scale(1.5)"
     //     img.style.transition = "transform 0.25s ease"
     // )
-    return (
+    const hover = useMediaQuery('(hover: hover)');
+    const [clicked, setClicked] = useState(false);
+
+    return (hover ? 
         <div className='design'>
-                <div className="thumbnail-box">
-                    <div className="overlay" >
-                        <div className = "overlay-top"> 
+            <div className="thumbnail-box">
+                <div className="overlay" >
+                    <div className = "overlay-top"> 
                         <h1 className="des_label">{design.title}</h1>
                         <p className="des_label">{design.author}</p>
-                        </div>
-                        <br/>
-                        <div className="icons">
-                            <a href={design.design_path} download className='link'><img src={download_icon} className="downloadIcon"/></a>
-                            <img onClick={() => onClick(design.id)} className="expandIcon" src={expandIcon}/>
-                        </div>
                     </div>
-                    <img src={design.image_path} className='thumbnail'/>
+                    <a href={design.design_path} download><img src={download_icon} className="downloadIcon"/></a>
+                    <img onClick={() => onClick(design.id)} className="expandIcon" src={expandIcon}/>
                 </div>
-            
+                <img src={design.image_path} className='thumbnail'/>
+            </div>
+        </div>
+        :
+        <div className='design'>
+            <div className="thumbnail-box" onClick={() => setClicked(!clicked)}>
+                <TouchOverlay design={design} onClick={onClick} clicked={clicked}/>
+                <img src={design.image_path} className='thumbnail'/>
+            </div>
         </div>
     );
 }
 
+const TouchOverlay = ({design, onClick, clicked}) => {
+    
+    return (clicked) ? 
+        <div className="overlay" >
+            <div className = "overlay-top"> 
+                <h1 className="des_label">{design.title}</h1>
+                <p className="des_label">{design.author}</p>
+            </div>
+            <a href={design.design_path} download><img src={download_icon} className="downloadIcon"/></a>
+            <img onClick={() => onClick(design.id)} className="expandIcon" src={expandIcon}/>
+        </div> : ""
+}
+
 export default Design
+
